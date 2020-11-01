576b44c20a8d3c9e523a21d0312216c10f97f83d
Keith Hazelton <khazelton@gmail.com>
2020-11-01 10:03:11 -0600

    clientBuilder errors

--- src/test/java/org/aktis/scimFun/ScimFunApplicationTests.java
+++ src/test/java/org/aktis/scimFun/ScimFunApplicationTests.java
@@ -1,8 +1,18 @@
 package org.aktis.scimFun;
 
+import com.unboundid.scim2.client.ScimService;
+import com.unboundid.scim2.common.types.Email;
+import com.unboundid.scim2.common.types.Name;
+import com.unboundid.scim2.common.types.UserResource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import java.util.Collections;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ScimFunApplicationTests {
@@ -12,9 +22,26 @@ class ScimFunApplicationTests {
 		assertThat(true);
 
 		// code for tests goes here
+		ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("https://example.com/scim/v2");
+		ScimService scimService = new ScimService(target);
 
-
+		// Create a user
+		UserResource user = new UserResource();
+		user.setUserName("babs");
+		user.setPassword("secret");
+		Name name = new Name()
+				.setGivenName("Barbara")
+				.setFamilyName("Jensen");
+		user.setName(name);
+		Email email = new Email()
+				.setType("home")
+				.setPrimary(true)
+				.setValue("babs@example.com");
+		user.setEmails(Collections.singletonList(email));
 
 	}
 
 }
+
